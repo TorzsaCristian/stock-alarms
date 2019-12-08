7626bec473bd312010b8bc6e4bb9bb18e26824bf
Torzsa Cristian <torzsacristian@gmail.com>
2019-12-09 00:46:01 +0200

    First commit. Todo app, withput user registration

---
+++ src/main/java/com/torzsa/stockalarms/model/Todo.java
@@ -0,0 +1,65 @@
+package com.torzsa.stockalarms.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    @Size(min = 10, message = "Enter at least 10 characters...")
+    private String description;
+
+    private Date targetDate;
+
+    public Todo() {
+        super();
+    }
+
+    public Todo(String username, String description, Date targetDate, boolean isDone) {
+        super();
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+}
